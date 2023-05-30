6aff83678576321bed0457c713ae2635ee75d86a
root <root@a3elp49.lnxero1.boe>
2023-05-30 15:48:13 +0200

    IBM Z Support

---
+++ hack/build.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+# builds the operator and its OLM catalog index and pushes it to quay.io.
+#
+# by default, the built catalog index is tagged with
+# `quay.io/projectquay/container-security-operator-index:1.0.6-unstable`. you can override the
+# tag alone by exporting TAG before executing this script.
+#
+# To push to your own registry, override the REGISTRY and NAMESPACE env vars,
+# i.e:
+#   $ REGISTRY=quay.io NAMESPACE=yourusername ./hack/build.sh
+#
+# REQUIREMENTS:
+#  * a valid login session to a container registry.
+#  * `docker`
+#  * `yq`
+#  * `opm`
+#
+# NOTE: this script will modify the following files:
+#  - bundle/manifests/container-security-operator.clusterserviceversion.yaml
+#  - bundle/metadata/annotations.yaml
+# if `git` is available it will be used to checkout changes to the above files.
+# this means that if you made any changes to them and want them to be persisted,
+# make sure to commit them before running this script.
+set -e
+
+export OPERATOR_NAME='container-security-operator-test'
+export REGISTRY=${REGISTRY:-'quay.io'}
+export NAMESPACE=${NAMESPACE:-'projectquay'}
+export TAG=${TAG:-'v1.0.6'}
+export CSV_PATH=${CSV_PATH:-'bundle/manifests/container-security-operator.clusterserviceversion.yaml'}
+export ANNOTATIONS_PATH=${ANNOTATIONS_PATH:-'bundle/metadata/annotations.yaml'}
+
+function cleanup {
+	# shellcheck disable=SC2046
+	if [ -x $(command -v git >/dev/null 2>&1) ]; then
+		git checkout "${CSV_PATH}" >/dev/null 2>&1
+		git checkout "${ANNOTATIONS_PATH}" >/dev/null 2>&1
+	fi
+}
+
+trap cleanup EXIT
+
+# prints pre-formatted info output.
+function info {
+	echo "INFO $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+# prints pre-formatted error output.
+function error {
+	>&2 echo "ERROR $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+function digest() {
+	declare -n ret=$2
+	IMAGE=$1
+        echo $IMAGE
+	docker pull "${IMAGE}"
+	# shellcheck disable=SC2034
+	ret=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}")
+}
+
+docker build -t "${REGISTRY}/${NAMESPACE}/container-security-operator:${TAG}" .
+docker push "${REGISTRY}/${NAMESPACE}/container-security-operator:${TAG}"
+digest "${REGISTRY}/${NAMESPACE}/container-security-operator:${TAG}" OPERATOR_DIGEST
+
+# need exporting so that yq can see them
+export OPERATOR_DIGEST
+
+# prepare operator files, then build and push operator bundle and catalog
+# index images.
+
+yq eval -i '
+	.metadata.name = strenv(OPERATOR_NAME) |
+	.metadata.annotations.version = strenv(TAG) |
+	.metadata.annotations.containerImage = strenv(OPERATOR_DIGEST) |
+	del(.spec.replaces) |
+	.spec.install.spec.deployments[0].name = strenv(OPERATOR_NAME) |
+	.spec.install.spec.deployments[0].spec.template.spec.containers[0].image = strenv(OPERATOR_DIGEST)
+	' "${CSV_PATH}"
+
+cat ${CSV_PATH} > csv_file
+yq eval -i '
+	.annotations."operators.operatorframework.io.bundle.channel.default.v1" = "test" |
+	.annotations."operators.operatorframework.io.bundle.channels.v1" = "test"
+	' "${ANNOTATIONS_PATH}"
+
+docker build -f ./bundle/Dockerfile -t "${REGISTRY}/${NAMESPACE}/container-security-operator-bundle:${TAG}" ./bundle
+docker push "${REGISTRY}/${NAMESPACE}/container-security-operator-bundle:${TAG}"
+digest "${REGISTRY}/${NAMESPACE}/container-security-operator-bundle:${TAG}" BUNDLE_DIGEST
+
+opm index add --build-tool docker --bundles "${BUNDLE_DIGEST}" --tag "${REGISTRY}/${NAMESPACE}/container-security-operator-index:${TAG}"
+docker push "${REGISTRY}/${NAMESPACE}/container-security-operator-index:${TAG}"
---
+++ hack/teardown.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# teardown the operator deployed by hack/deploy.sh
+#
+# REQUIREMENTS:
+#  * a valid login session to an OCP cluster, with cluster admin privileges
+#  * `yq` cmd line tool
+#  * `oc` cmd line tool
+
+# prints pre-formatted info output.
+function info {
+	echo "INFO $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+export OPERATOR_PKG_NAME=${OPERATOR_PKG_NAME:-'container-security-operator-test'}
+export OG_PATH=${OG_PATH:-'./bundle/cso.operatorgroup.yaml'}
+export SUBSCRIPTION_PATH=${SUBSCRIPTION_PATH:-'./bundle/cso.subscription.yaml'}
+
+info 'uninstalling operator'
+oc delete operatorgroup "$(yq e '.metadata.name' "${OG_PATH}")"
+oc delete subscription "$(yq e '.metadata.name' "${SUBSCRIPTION_PATH}")"
+oc delete csv "${OPERATOR_PKG_NAME}"
+
+info 'deleting catalog source'
+oc delete catsrc "${OPERATOR_PKG_NAME}" -n openshift-marketplace
